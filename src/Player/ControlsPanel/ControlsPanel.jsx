3dca6ef775edfdc24aa037b712260e54d085de3e
Shihua Chen <shihua.chen@thoughtworks.com>
2023-02-27 15:37:33 +0800

    refactor: extract variable.scss

--- src/Player/ControlsPanel/ControlsPanel.jsx
+++ src/Player/ControlsPanel/ControlsPanel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {ControlsButton} from "./components/ControlsButton";
 import {Progress} from "./components/Progress";
 import {VolumePanel} from "./components/VolumePanel";
-import './ControlsPanel.scss'
+import './ControlsPanel.scss';
 
 export const ControlsPanel = () => {
     return (
--- src/Player/ControlsPanel/components/ControlsButton.jsx
+++ src/Player/ControlsPanel/components/ControlsButton.jsx
@@ -6,9 +6,7 @@ export const ControlsButton = () => {
         <div className="controls-button">
             <button className="play-button" id="play">
                 <svg viewBox="0 0 36 36" height="100%">
-                    <path className="svg-fill"
-                          d="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z"
-                          fill="#000000"></path>
+                    <path d="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z"></path>
                 </svg>
             </button>
         </div>
