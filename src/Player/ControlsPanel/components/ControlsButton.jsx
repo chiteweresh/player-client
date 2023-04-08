56278530b58c6585f22bb9c41c3a0ba994b7b0ac
Shihua Chen <shihua.chen@thoughtworks.com>
2023-04-08 23:57:32 +0800

    refactor: extract svg files

--- src/Player/ControlsPanel/components/ControlsButton.jsx
+++ src/Player/ControlsPanel/components/ControlsButton.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './ControlsButton.scss';
 import PropTypes from 'prop-types';
+import play from '../../../svg/play.svg';
+import pause from '../../../svg/pause.svg';
+import forward from '../../../svg/forward.svg';
+import rewind from '../../../svg/rewind.svg';
 
 const ControlsButton = ({ onPlayPause, playing }) => (
   <div className="controls-button">
+    <img src={forward} alt="pause" />
     <button type="button" className="play-button" onClick={onPlayPause}>
       {!playing ? (
-        <svg viewBox="0 0 36 36" height="100%">
-          <path d="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z" />
-        </svg>
+        <img src={play} alt="play" />
       ) : (
-        <svg viewBox="0 0 36 36" height="100%">
-          <path d="M 12,26 16,26 16,10 12,10 z M 21,26 25,26 25,10 21,10 z" />
-        </svg>
+        <img src={pause} alt="pause" />
       )}
     </button>
+    <img src={rewind} alt="pause" />
   </div>
 );
 
